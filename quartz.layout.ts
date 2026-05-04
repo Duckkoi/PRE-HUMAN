a68a20f7d30ef360a7aaaf87e627797bfb83568a
Isa <mrduckpersona@gmail.com>
2026-05-05 01:56:31 +0200

    vault backup: 2026-05-05 01:56:31

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
@@ -12,6 +13,7 @@ export const sharedPageComponents: SharedLayout = {
     },
   }),
 }
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
@@ -29,7 +31,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Flex({
       components: [
         {
-          Component: [Component.Search](http://Component.Search)(),
+          Component: Component.Search(),
           grow: true,
         },
         { Component: Component.Darkmode() },
@@ -44,7 +46,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Backlinks(),
   ],
 }
-// components for pages that display lists of pages  (e.g. tags or folders)
+
+// components for pages that display lists of pages (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
@@ -53,7 +56,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.Flex({
       components: [
         {
-          Component: [Component.Search](http://Component.Search)(),
+          Component: Component.Search(),
           grow: true,
         },
         { Component: Component.Darkmode() },
